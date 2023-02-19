1ffb5caf573bbe73c8c8505e8d4424d17a033f01
Jinho <dfghcvb11@naver.com>
2023-02-19 21:22:20 +0900

    hello spring-concurrency!

---
+++ src/test/java/com/binaryho/springconcurrency/SpringConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binaryho.springconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
