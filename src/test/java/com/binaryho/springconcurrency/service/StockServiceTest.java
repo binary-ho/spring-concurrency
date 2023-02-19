7cebe47decff54e5e599c870482d050d26c68157
Jinho <dfghcvb11@naver.com>
2023-02-19 23:19:06 +0900

    test : 동시에 여러 요청을 수행할 때 실패 테스트 구현

--- src/test/java/com/binaryho/springconcurrency/service/StockServiceTest.java
+++ src/test/java/com/binaryho/springconcurrency/service/StockServiceTest.java
@@ -5,6 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.binaryho.springconcurrency.domain.Stock;
 import com.binaryho.springconcurrency.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +19,7 @@ class StockServiceTest {
 
     public final long STOCK_QUANTITY = 100L;
     public final long PRODUCT_ID = 1L;
+    public final int THREAD_POOL_SIZE = 32;
 
     @Autowired private StockService stockService;
     @Autowired private StockRepository stockRepository;
@@ -42,4 +46,28 @@ class StockServiceTest {
 
         assertThat(stock.getQuantity()).isEqualTo(STOCK_QUANTITY - decreaseQuantity);
     }
+
+    @Test
+    public void 동시에_여러_요청을_처리할_수_있다() throws InterruptedException {
+        long decreaseQuantity = 1L;
+        int threadCount = (int) STOCK_QUANTITY;
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(PRODUCT_ID, decreaseQuantity);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        Stock stock = stockRepository.findById(PRODUCT_ID).orElseThrow();
+        assertThat(stock.getQuantity()).isEqualTo(0L);
+    }
+
 }
